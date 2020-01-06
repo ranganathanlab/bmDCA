84b4bb61808840274492ee59dca2decb392bf829
sudorook <daemon@nullcodon.com>
2020-01-06 03:20:48 -0600

    tweak axis label for gnuplot script

--- scripts/plot_stat.sh
+++ scripts/plot_stat.sh
@@ -7,7 +7,7 @@ plot "errorlist.txt" u 3:4 w p pt 0 title "1 point marginals"
 replot "my_corr.dat" u 1:2 w p pt 0 title "correlations"
 set grid
 set xlabel "MSA"
-set ylabel "MC "
+set ylabel "MCMC"
 set term jpeg
 set out "cfr_stat.jpeg"
 rep
--- scripts/plot_stat_reg.sh
+++ scripts/plot_stat_reg.sh
@@ -7,7 +7,7 @@ plot "errorlist.txt" u 3:8 w p pt 0 title "1 point marginals"
 replot "my_corr.dat" u 1:3 w p pt 0 title "correlations"
 set grid
 set xlabel "MSA"
-set ylabel "MC "
+set ylabel "MCMC"
 set term jpeg
 set out "cfr_stat_reg.jpeg"
 rep
