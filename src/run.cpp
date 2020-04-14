a65c3d654ed76bc9b8f03eb761af1b7a233fbdcb
sudorook <daemon@nullcodon.com>
2020-04-13 20:31:07 -0500

    minor spacing in message

--- src/run.cpp
+++ src/run.cpp
@@ -767,7 +767,7 @@ Sim::run(void)
     t_wait = t_wait_0;
     delta_t = delta_t_0;
   } else if (step_offset >= step_max) {
-    std::cout << "step"  << step_max << " already reached... done." << std::endl;
+    std::cout << "step "  << step_max << " already reached... done." << std::endl;
     return;
   } else {
     burnRNG();
